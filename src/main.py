a04c2c772f573b0b85ce038dfac18431e977c450
Victor Mak <contact@htetaung.com>
2024-03-12 13:31:14 +0800

    feat: data post api setup

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pydantic import BaseModel
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 import os
 
 load_dotenv()
 
 app = FastAPI()
 uri = os.getenv("URI")
+
+class RequestEntity(BaseModel):
+    city: str
+    country: str
+    place: str
+    budget: int
+    date: str
+    adventurous: bool
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
@@ -27,4 +36,7 @@ def get_status_check():
 def post_status_check():
     return {"Request Type": "POST", "API Working Status": "Up and Running!", "Mongo Status": ping_mongodb()}
 
-
+@app.post("/get_data")
+async def get_data(city: str = Form(...), country: str = Form(...), place: str = Form(...), budget: int = Form(...), date: str = Form(...), adventurous: bool = Form(...)):
+    request = RequestEntity(city=city, country=country, place=place, budget=budget, date=date, adventurous=adventurous)
+    return {"Request Type": "POST", "City": request.city, "Country": request.country, "Place": request.place, "Budget": request.budget, "Date": request.date, "Adventurous": request.adventurous}
